34743507c3af0e86f5bb135705bbc50a61d7f843
xF14x | Sulaiman ALMohawis <>
2023-02-09 00:00:00 +0300

    Create a folder in which to save logs files

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 # Programming By Sulaiman AL-Mohawis Twitter:f14commander Github:xF14x
-from os import system
+import os
 from list import list
 from datetime import datetime
 
 date = datetime.now()
 
+FolderName = "Logs" # You Can Change The Folder Name For Save Logs Files
+
+if not os.path.isdir(FolderName):
+    os.makedirs(FolderName)
+else:
+    pass
+
 for ping in list:
-    response = system(f'ping -c 1 {ping}')
+    response = os.system(f'ping -c 1 {ping}')
 
     if response == 0:
-        with open(f'{ping}.txt', 'a') as log:
-            log.write(f'{ping} is up in {date}\n')
+        with open(f'{FolderName}/{ping}.txt', 'a') as log:
+            log.write(f'{FolderName}/{ping} is up in {date}\n')
     else:
-        with open(f'{ping}.txt', 'a') as log:
-            log.write(f'{ping} is down in {date}\n')
+        with open(f'{FolderName}/{ping}.txt', 'a') as log:
+            log.write(f'{FolderName}/{ping} is down in {date}\n')
